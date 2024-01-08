15c9c0735576513eb8ca152de881b5f501b7bbd7
Pavel Vladimirovich Erokhin <pavel@Pavels-MacBook-Pro.local>
2024-01-08 03:05:34 +0100

    frontend

--- parking_lot/main.rb
+++ parking_lot/main.rb
@@ -9,10 +9,10 @@ require_relative './parking/parking_1_queue'
 
 logger = Logger.new(STDOUT)
 
-parking_with_1_queue = Parking1Queue.new(levels = 2,
-                            rows_in_level=5,
-                            places_in_row = 8,
-                            queue_max_size = 50,
+parking_with_1_queue = Parking1Queue.new(levels = 1,
+                            rows_in_level=4,
+                            places_in_row = 9,
+                            queue_max_size = 10,
                             vehicles_arrive_hours_distribution = 0.1..0.15,
                             leave_parking_hours_distribution = 9.5..11.5)
 
@@ -22,7 +22,8 @@ gui_server = GuiServer.new(parking = parking_with_1_queue,
 gui_server.open_browser
 gui_server.listen_and_serve
 
-sleep(2) # wait while the browser is opening
+sleep(1) # wait while the browser is opening
 
 # run the simulation
 parking_with_1_queue.run
+
